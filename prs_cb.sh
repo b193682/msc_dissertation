2e49c391286753fc54de3bd7f8cde1efdf690db2
b193682 <katie.r.robinson2003@outlook.com>
2026-08-09 16:03:03 +0100

    Create prs_cb.sh
    Cannabis use disorder PRS shell script

---
+++ prs_cb.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# Polygenic risk score analysis for cannabis use disorder, using C+T threshold derived from Choi and O'Reilly (2020) https://doi.org/10.1038/s41596-020-0353-1
+# PRS calculated using PRSice-2 v2.1.11 https://choishingwan.github.io/PRSice/
+# Discovery dataset from Johnson et al. (2020) https://doi.org/10.1016/S2215-0366(20)30339-4
+# Job scheduler instructions to run from current working directory, request 32GB memory, and set a maximum runtime of 24 hours.
+#$ -cwd
+#$ -l h_vmem=32G
+#$ -l h_rt=24:00:00
+# Specifies the output and erorr file locations for the output.
+#$ -o /home/s2223599/gwas_pipeline/logs/prs_bip.log
+#$ -o /home/s2223599/gwas_pipeline/logs/prs_cud.log
+#$ -e /home/s2223599/gwas_pipeline/logs/prs_cud.err
+
+#Initialise the modules system to work in qsub job scripts.
+. /etc/profile.d/modules.sh
+module load igmm/apps/PRSice/2.1.11
+
+cd /exports/eddie/scratch/s2223599/gwas_pipeline
+
+# PRS for cannabis use disorder against all four SPQ-B phenotypes
+
+# Total score
+PRSice \
+  --base data/CUD_EUR_casecontrol_public_11.14.2020.gz \
+  --target qc/GS_qc_final_rs \
+  --binary-target F \
+  --pheno-file data/pheno_total.txt \
+  --cov-file data/covars_header.covar \
+  --stat Beta \
+  --snp SNP \
+  --A1 A1 \
+  --A2 A2 \
+  --pvalue P \
+  --bar-levels 0.001,0.05,0.1,0.2,0.3,0.4,0.5,1 \
+  --fastscore \
+  --out prs/GS_prs_cud_total
+
+# Cognitive-perceptual
+PRSice \
+  --base data/CUD_EUR_casecontrol_public_11.14.2020.gz \
+  --target qc/GS_qc_final_rs \
+  --binary-target F \
+  --pheno-file data/pheno_cogper.txt \
+  --cov-file data/covars_header.covar \
+  --stat Beta \
+  --snp SNP \
+  --A1 A1 \
+  --A2 A2 \
+  --pvalue P \
+  --bar-levels 0.001,0.05,0.1,0.2,0.3,0.4,0.5,1 \
+  --fastscore \
+  --out prs/GS_prs_cud_cogper
+
+# Interpersonal
+PRSice \
+  --base data/CUD_EUR_casecontrol_public_11.14.2020.gz \
+  --target qc/GS_qc_final_rs \
+  --binary-target F \
+  --pheno-file data/pheno_inter.txt \
+  --cov-file data/covars_header.covar \
+  --stat Beta \
+  --snp SNP \
+  --A1 A1 \
+  --A2 A2 \
+  --pvalue P \
+  --bar-levels 0.001,0.05,0.1,0.2,0.3,0.4,0.5,1 \
+  --fastscore \
+  --out prs/GS_prs_cud_inter
+
+# Disorganisation
+PRSice \
+  --base data/CUD_EUR_casecontrol_public_11.14.2020.gz \
+  --target qc/GS_qc_final_rs \
+  --binary-target F \
+  --pheno-file data/pheno_disorg.txt \
+  --cov-file data/covars_header.covar \
+  --stat Beta \
+  --snp SNP \
+  --A1 A1 \
+  --A2 A2 \
+  --pvalue P \
+  --bar-levels 0.001,0.05,0.1,0.2,0.3,0.4,0.5,1 \
+  --fastscore \
+  --out prs/GS_prs_cud_disorg
